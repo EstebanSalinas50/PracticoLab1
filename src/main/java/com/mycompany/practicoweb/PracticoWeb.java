c6fcd1c2890754b86d38d96b84eec1d45cc6484d
Blas Haberland <haberlandblas@gmail.com>
2024-03-19 11:33:03 -0300

    Agrego sout

--- src/main/java/com/mycompany/practicoweb/PracticoWeb.java
+++ src/main/java/com/mycompany/practicoweb/PracticoWeb.java
@@ -10,5 +10,6 @@ public class PracticoWeb {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo en español!");
+        System.out.println("Prueba de push");
     }
 }
